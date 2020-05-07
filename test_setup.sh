1ebfb652eb6eb727e711dc0ba17c46e1678670ae
Alexandru Moise <00moses.alexander00@gmail.com>
2020-05-07 18:50:21 +0200

    introduce test_setup.sh script
    This script creates 3 network namespaces and 3 tunnel
    virtual ethernet devices for each, the 3 veth devices
    are bridged in the default namespace. All servers are
    able to communicate with one another on the 10.0.0.0/24
    network.
    
    The controller ip address is 10.0.0.1
    The server ip addresses are assigned 10.0.0.10<server instance number>
    
    i.e.
    
    Server 1: 10.0.0.101
    Server 2: 10.0.0.102
    Server 3: 10.0.0.103
    
    For each server a separate terminal window needs to be opened.
    To enter the namespace of each server you must run this command first:
    
    For server1:
    ip netns exec server1 bash
    
    Now running "ip addr" will only list the peer veth device for the given
    server, as that is the only interface assigned to each server's namespace:
    
    [root@nodeone server1]# ip addr
    1: lo: <LOOPBACK> mtu 65536 qdisc noop state DOWN group default qlen 1000
        link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
    5: server1_p@if6: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc noqueue state UP group default qlen 1000
        link/ether ca:9a:50:a2:41:8a brd ff:ff:ff:ff:ff:ff link-netnsid 0
        inet 10.0.0.101/24 scope global server1_p
           valid_lft forever preferred_lft forever
        inet6 fe80::c89a:50ff:fea2:418a/64 scope link
           valid_lft forever preferred_lft forever
    
    Once in the server's namespace, cd into the serverN directory:
    
    eg:
    
    cd server1/
    
    And run ./server.py script as usual.
    
    The network namespace isolation provides the ilusion that each server is
    on a separate machine.
    
    In the default namespace the veth devices are all bridged together  on br0:
    
    obsrwr@nodeone:~/work/p2preliablestorage$ brctl show
    bridge name     bridge id               STP enabled     interfaces
    br0             8000.d29e9ad1ca71       no              server1
                                                            server2
                                                            server3
    
    server1 is the veth interface whose peer interface in the "server1" namespace is server1_p.
    This is basically a tunnel interface.

---
+++ test_setup.sh
@@ -0,0 +1,35 @@
+set -x
+brctl addbr br0
+ip link add server1 type veth peer name server1_p
+ip link add server2 type veth peer name server2_p
+ip link add server3 type veth peer name server3_p
+ip netns add server1
+ip netns add server2
+ip netns add server3
+ip link set server1_p netns server1
+ip link set server2_p netns server2
+ip link set server3_p netns server3
+brctl addif br0 server1
+brctl addif br0 server2
+brctl addif br0 server3
+
+ip link set dev br0 up
+ip link set dev server1 up
+ip link set dev server2 up
+ip link set dev server3 up
+
+ip addr add 10.0.0.1/24 dev br0
+ip netns exec server1 ip link set dev server1_p up
+ip netns exec server2 ip link set dev server2_p up
+ip netns exec server3 ip link set dev server3_p up
+ip netns exec server1 ip addr add 10.0.0.101/24 dev server1_p
+ip netns exec server2 ip addr add 10.0.0.102/24 dev server2_p
+ip netns exec server3 ip addr add 10.0.0.103/24 dev server3_p
+
+mkdir server1 server2 server3
+cp sensor_simulator.py server.py server1/ 
+cp sensor_simulator.py server.py server2/ 
+cp sensor_simulator.py server.py server3/ 
+touch server1/id_file.txt
+touch server2/id_file.txt
+touch server3/id_file.txt
